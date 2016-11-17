9fb5513c3e8492839b6bb13ed6f9dd8249c28c78
imabu <mfumfu17@gmail.com>
2016-11-17 20:48:27 +0300

    Change href in tag.html2

--- ask_app/views.py
+++ ask_app/views.py
@@ -43,6 +43,6 @@ def tag(request,tag):
 	questions=[]
 	t=get_template('tag.html')
 	for i in range(1,10):
-		questions.append({'question_id':question_id,'title': 'title'+str(i), 'text':'text','author': 'user '+str(i),'rating':i, 'tags':[str(i), 'tag'+str(i)]},)
+		questions.append({'question_id':i,'title': 'title'+str(i), 'text':'text','author': 'user '+str(i),'rating':i, 'tags':[str(i), 'tag'+str(i)]},)
 	context={'questions':questions,'tag':tag}
 	return render(request,'tag.html',context)
