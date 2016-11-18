e5c8b26ac156e26fd04c5b83f23882052956c736
imabu <mfumfu17@gmail.com>
2016-11-19 01:14:47 +0300

    models&admin

--- ask_app/admin.py
+++ ask_app/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from ask_app import models
 # Register your models here.
 class QuestionAdmin(admin.ModelAdmin):
-    list_display('title','text','author','tags')
+    display_list('title','text','author','tags')
 class AnswerAdmin(admin.ModelAdmin):
     list_display('text','author','tags')
 admin.site.register(models.Question,QuestionAdmin)
