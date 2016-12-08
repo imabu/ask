d43196147ad93119c0600e0a7b1a211cfd237411
imabu <mfumfu17@gmail.com>
2016-12-08 21:58:07 +0300

    models

--- ask_app/admin.py
+++ ask_app/admin.py
@@ -1,16 +1,35 @@
 from django.contrib import admin
 from ask_app import models
+from django.contrib import admin
 # Register your models here.
+
 class QuestionAdmin(admin.ModelAdmin):
-    list_display=('title',)
+    list_display=('title','author','rating',)
+
+
 class AnswerAdmin(admin.ModelAdmin):
-    list_display=('text',)
+    list_display=('author','text','rating',)
+
+
 class TagAdmin(admin.ModelAdmin):
     list_display=('value',)
-#class ProfileAdmin(admin.ModelAdmin):
-    #list_display=('login',)
+
+
+class ProfileAdmin(admin.ModelAdmin):
+    list_display=('nickname',)
+
+
+class LikeQuestionAdmin(admin.ModelAdmin):
+    list_display=('value',)
+
+
+class LikeAnswerAdmin(admin.ModelAdmin):
+    list_display=('value',)
+
 
 admin.site.register(models.Question,QuestionAdmin)
 admin.site.register(models.Answer,AnswerAdmin)
 admin.site.register(models.Tag,TagAdmin)
-#admin.site.register(models.Profile,ProfileAdmin)
+admin.site.register(models.Profile,ProfileAdmin)
+admin.site.register(models.LikeQuestion,LikeQuestionAdmin)
+admin.site.register(models.LikeAnswer,LikeAnswerAdmin)
--- ask_app/models.py
+++ ask_app/models.py
@@ -7,6 +7,11 @@ from django.contrib.auth.models import User, UserManager
 #makemigrations
 class Tag(models.Model):
 	value=models.CharField(max_length=15,verbose_name=u'Tag')
+	class Meta:
+		verbose_name=u'Tag'
+		verbose_name_plural=u'Tags'
+	def __str__(self):
+		return "{}".format(self.value)
 class Profile(models.Model):
 	profile = models.OneToOneField(User)
 	nickname=models.CharField(max_length=30)
@@ -14,8 +19,8 @@ class Profile(models.Model):
 	class Meta:
 		verbose_name=u'Profile'
 		verbose_name_plural=u'Profiles'
-	def __unicode__(self):
-		return self.nickname	
+	def __str__(self):
+		return  "{}".format(self.nickname)
 class Question(models.Model):
 	title= models.CharField(max_length=100,verbose_name=u'Title')
 	text= models.TextField(verbose_name=u'Text')
@@ -25,8 +30,8 @@ class Question(models.Model):
 	class Meta:
 		verbose_name=u'Question'
 		verbose_name_plural=u'Questions'
-	def __unicode__(self):
-		return self.title
+	def __str__(self):
+		return "{}".format(self.title)
 
 class Answer(models.Model):
 	text= models.TextField(verbose_name=u'Text')
@@ -37,19 +42,18 @@ class Answer(models.Model):
 	class Meta:
 		verbose_name=u'Answer'
 		verbose_name_plural=u'Answers'
-	def __unicode__(self):
-		return self.text
+	def __str__(self):
+		return "{} {}".format(self.question.title)
 class LikeQuestion(models.Model):
 	user=models.ForeignKey('Profile',verbose_name=u'User')
 	question=models.ForeignKey('Question')
 	AVAILABLE_VALUE=((1,'like'),(-1,'dislike'))
 	value=models.IntegerField(choices=AVAILABLE_VALUE, blank=True, null=True)
-	def __unicode__(self):
-		return self.value
+	def __str__(self):
+		return "{} {} {}".format(self.question.title,self.user.nickname,self.value)
 class LikeAnswer(models.Model):
 	user=models.ForeignKey('Profile',verbose_name=u'User')
 	answer=models.ForeignKey('Answer')
-	AVAILABLE_VALUE=((1,'like'),(-1,'dislike'))
-	value=models.IntegerField(choices=AVAILABLE_VALUE, blank=True, null=True)
+	value=models.IntegerField(blank=True, null=True)
 	def __unicode__(self):
-		return self.text
+		return self.value
